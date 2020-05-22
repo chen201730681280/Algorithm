67e902a998dc3f308f5119d9c4469759f99afb01
chenyx <40078208+chen201730681280@users.noreply.github.com>
2020-05-22 14:44:56 +0800

    配c++环境

---
+++ jzoffer/1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=0;
+    cin>>a;
+    cout<<"1"<<endl;
+    cout<<a<<endl;
+    system("pause");
+   
+}
+
+// #include <stdio.h>
+// int main(){
+//    char name[100];
+//    printf("What is your name?\n");
+//    scanf("%s",name);
+//    printf("Hello,%s,nice to meet you!\n",name);
+// }
